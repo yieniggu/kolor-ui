e81ff33ae1e46a57d05f55fe26983d0eed79cf3d
Bastiloko <bastianandresxb@gmail.com>
2022-04-07 13:31:56 -0400

    renamed main app file

---
+++ src/KolorApp.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Provider } from "react-redux";
+import { AppRouter } from "./routers/AppRouter";
+import { store } from "./store/store";
+
+export const KolorApp = () => {
+  return (
+    <div>
+      <Provider store={store}>
+        <AppRouter />
+      </Provider>
+    </div>
+  );
+};
