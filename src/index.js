b8ee61c5fa89eb015fa9c63dce8420e918a9c7a2
Bastiloko <bastianandresxb@gmail.com>
2022-04-07 13:32:13 -0400

    rollback to react v17.0.2

--- src/index.js
+++ src/index.js
@@ -1,10 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { KolorApp } from "./App";
+import { KolorApp } from "./KolorApp";
+import "./styles.css";
 
-import { createRoot } from "react-dom/client";
-
-const container = document.getElementById("root");
-const root = createRoot(container);
-
-root.render(<KolorApp tab="home" />);
+ReactDOM.render(<KolorApp />, document.getElementById("root"));
