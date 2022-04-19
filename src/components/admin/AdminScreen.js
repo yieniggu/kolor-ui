b919d36264b7ef7130c83d00d30ed9c31c1d884b
Bastiloko <bastianandresxb@gmail.com>
2022-04-18 20:50:28 -0400

    added tabs

--- src/components/admin/AdminScreen.js
+++ src/components/admin/AdminScreen.js
@@ -1,10 +1,30 @@
 import React from "react";
+import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
+import "react-tabs/style/react-tabs.css";
+import { NFTManagement } from "./NFT/NFTManagement";
+
 import { NFTMintingPanel } from "./NFT/NFTMintingPanel";
+import { OffsetManagement } from "./offset/OffsetManagement";
 
 export const AdminScreen = () => {
   return (
-    <div className="mt-4">
-      <NFTMintingPanel />
+    <div className="container mt-4">
+      <Tabs>
+        <TabList>
+          <Tab>Minting</Tab>
+          <Tab>Land Management</Tab>
+          <Tab>Offset Requests</Tab>
+        </TabList>
+        <TabPanel>
+          <NFTMintingPanel />
+        </TabPanel>
+        <TabPanel>
+          <NFTManagement />
+        </TabPanel>
+        <TabPanel>
+          <OffsetManagement />
+        </TabPanel>
+      </Tabs>
     </div>
   );
 };
